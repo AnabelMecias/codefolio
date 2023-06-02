b23166960aaaa61545f985bcce851d67e8f1446f
AnabelMecias <anabel.mecias19981111@gmail.com>
2023-06-02 15:02:38 -0400

    Ecercises to practice

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code to be executed when the program starts
+            Console.WriteLine("Hello World!");
+
+            // Wait for user input before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
