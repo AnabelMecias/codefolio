268cc9b9e4bbfcfaf029e224af301b52dcf28498
AnabelMecias <anabel.mecias19981111@gmail.com>
2023-06-02 15:06:04 -0400

    Deleting unnesasary things

--- Program.cs
+++ Program.cs
@@ -6,11 +6,7 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
-            // Code to be executed when the program starts
-            Console.WriteLine("Hello World!");
-
-            // Wait for user input before closing the console window
-            Console.ReadKey();
+            
         }
     }
 }
